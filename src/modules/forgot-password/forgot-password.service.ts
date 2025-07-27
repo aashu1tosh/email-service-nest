71c86b59c55e583f4ccbc956b001cd4911d7d8b4
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-07-27 18:54:27 +0545

    feat(forgot-password): refactor publishForgotPasswordEmail to use config service for URL construction

--- src/modules/forgot-password/forgot-password.service.ts
+++ src/modules/forgot-password/forgot-password.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MailService } from '../mail/mail.service';
 
 @Injectable()
 export class ForgotPasswordService {
     constructor(
         private readonly mailService: MailService,
+        private readonly configService: ConfigService,
     ) { }
 
     async publishForgotPasswordEmail(data: any) {
         console.log("🚀 ~ ForgotPasswordService ~ publishForgotPasswordEmail ~ data:", data)
-        const { to, token } = data;
-
+        const { to, resetToken: token } = data;
+        const frontendUrl = this.configService.get<string>('FRONTEND_URL');
+        const restorePasswordPath = this.configService.get<string>('RESTORE_PASSWORD_PATH');
+        const fullPath = `${frontendUrl}${restorePasswordPath}/${token}`;
         // Construct the email content
         const subject = 'Password Reset Request';
         const html = `<p>To reset your password, please click the link below:</p>
-                      <a href="https://yourapp.com/reset-password?token=${token}">Reset Password</a>`;
+                      <a href="${fullPath}">Reset Password</a>`;
 
         // Send the email using MailService
         await this.mailService.sendMail({
